16e3a80945fbef9ea7551f185c0320fcd83d2e33
ch narendra <your.narendra736@gmail.com>
2013-04-17 16:14:55 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmpApp::Application.config.secret_token = '65fc79aadc86edde5e7250f9c073864f866566b513f6fe1066806b6f54772fe9e56cb919ef14f41f8f2040c322c2d55b6962f927fb1147d27fbcc018b18f5488'
